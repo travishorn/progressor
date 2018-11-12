4ae999f36eddc37637c845c2f35a3bacca9d0f03
Travis Horn <travis@travishorn.com>
2018-11-12 10:19:46 -0600

    Support browsers with >1% usage

--- webpack.config.js
+++ webpack.config.js
@@ -11,4 +11,18 @@ module.exports = {
     d3: 'd3',
   },
   mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
 };
