ff1a663e9a0ebaba99d3990abb661d5f4277c6bb
Travis Horn <travis@travishorn.com>
2018-11-12 11:36:34 -0600

    Demo JS restructuring

--- demo/demo.js
+++ demo/demo.js
@@ -6,10 +6,10 @@ const p3 = new Progressor('#styled');
 const p4 = new Progressor('#evented');
 
 const btn = document.getElementById('btn');
+let val = 0;
 
 p1.update(0.25);
 p2.update(0.5);
 p3.update(0.75);
 
-let val = 0;
 btn.addEventListener('click', () => { p4.update(val += 0.1); });
