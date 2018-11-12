b67f35208b18325f19858f2f8d0fbd70e46426cc
Travis Horn <travis@travishorn.com>
2018-11-12 10:07:34 -0600

    Add size option

--- demo/demo.js
+++ demo/demo.js
@@ -1,4 +1,7 @@
 /* globals Progressor */
 
-const p = new Progressor('body');
-p.update(0.5);
+const p1 = new Progressor('body');
+const p2 = new Progressor('body', { size: 200 });
+
+p1.update(0.5);
+p2.update(0.2);
--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 import * as d3 from 'd3';
 
-export default function (selector) {
-  const size = 100;
+export default function (selector, opts) {
+  const defaults = {
+    size: 100,
+  };
+  const o = Object.assign(defaults, opts);
   const app = d3.select(selector);
   const chart = app
     .append('svg')
     .attr('class', 'progressor')
-    .attr('viewBox', `0 0 1000 ${size}`);
+    .attr('viewBox', `0 0 1000 ${o.size}`);
   const token = chart
     .append('rect')
     .attr('class', 'progressor-token')
-    .attr('width', size)
-    .attr('height', size);
+    .attr('width', o.size)
+    .attr('height', o.size);
   const x = d3
     .scaleLinear()
     .domain([0, 1])
-    .range([0, 1000 - size]);
+    .range([0, 1000 - o.size]);
 
   this.update = function updateProgressor(newValue) {
     token
