6f97509c73e42976811db311991f2dba40ca3b44
Travis Horn <travis@travishorn.com>
2018-11-13 10:00:26 -0600

    Add custom image option

--- demo/demo.js
+++ demo/demo.js
@@ -4,12 +4,17 @@ const p1 = new Progressor('#default');
 const p2 = new Progressor('#size', { size: 200 });
 const p3 = new Progressor('#styled');
 const p4 = new Progressor('#evented');
+const p5 = new Progressor('#customImage', {
+  image: 'horse.png',
+  size: 52,
+});
 
 const btn = document.getElementById('btn');
 let val = 0;
 
-p1.update(0.25);
-p2.update(0.5);
-p3.update(0.75);
+p1.update(0.2);
+p2.update(0.4);
+p3.update(0.6);
+p5.update(0.8);
 
 btn.addEventListener('click', () => { p4.update(val += 0.1); });
--- src/index.js
+++ src/index.js
@@ -4,21 +4,34 @@ export default function (selector, opts) {
   const defaults = {
     size: 100,
   };
+
   const o = Object.assign(defaults, opts);
+
   const app = d3.select(selector);
+
   const chart = app
     .append('svg')
     .attr('class', 'progressor')
     .attr('viewBox', `0 0 1000 ${o.size}`);
-  const token = chart
-    .append('rect')
-    .attr('class', 'progressor-token')
-    .attr('width', o.size)
-    .attr('height', o.size);
+
   const x = d3
     .scaleLinear()
     .domain([0, 1])
     .range([0, 1000 - o.size]);
+
+  let token;
+
+  if (o.image) {
+    token = chart.append('image')
+      .attr('xlink:href', o.image);
+  } else {
+    token = chart.append('rect');
+  }
+
+  token
+    .attr('class', 'progressor-token')
+    .attr('width', o.size)
+    .attr('height', o.size);
 
   this.update = function updateProgressor(newValue) {
     token
